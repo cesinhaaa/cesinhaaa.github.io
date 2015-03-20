323085b2c20c5a2c1ad93a187f92952932aa94d7
Cesar Oliveira <cesinha@cidadesonho.com>
2015-03-20 02:16:16 -0300

    JS
    JS

--- dir/js/theme.js
+++ dir/js/theme.js
@@ -30,16 +30,6 @@
 		    }
 		});
 		
-		// Get window size
-	    winH = $window.height();
-	    
-	    // Keep minimum height 550
-	    if(winH <= 550) {
-			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
 	    
 	    // Refresh Skrollr after resizing our sections
 	    s.refresh($('.homeSlide'));
